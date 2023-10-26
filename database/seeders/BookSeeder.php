5f2633f96d0d5ce326c952c1b69e5977407ced49
Rishabh <rishabhgupta1393@gmail.com>
2023-10-26 17:33:41 +0530

    ignore

--- database/seeders/BookSeeder.php
+++ database/seeders/BookSeeder.php
@@ -3,7 +3,7 @@
 namespace Database\Seeders;
 
 use App\Models\Book;
-use App\Services\EsService;
+// use App\Services\EsService;
 use GuzzleHttp\Client;
 use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
@@ -19,11 +19,11 @@ class BookSeeder extends Seeder
         $stateQuery = $client->get('https://fakerapi.it/api/v1/books?_quantity=1000');
         $Data = $stateQuery->getBody();
         $Data = json_decode($Data, true)['data'];
-        $es = new EsService();
+        // $es = new EsService();
         foreach ($Data as  $value) {
 
             Book::create($value);
-            $es->create($value);
+            // $es->create($value);
 
         }
 
